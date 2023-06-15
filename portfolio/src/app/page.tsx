9ae5c103ac75b51a991f3d0cf2ba359cd3a25c62
Zenky <kezon.lacheteau@epitech.eu>
2023-06-15 22:38:30 +0200

    test serve

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center">
     {/*   <NavBar className="fixed" /> */}
-      <h1 className="text-9xl font-bold absolute">Hello World !</h1>
+     {/* <h1 className="text-9xl font-bold absolute">Hello World !</h1>*/}
       <Canvas className="w-full" />
     </div>
   );
