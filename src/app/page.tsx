931226acdb48099bd323b4640c05454c87c14207
Zenky <kezon.lacheteau@epitech.eu>
2023-06-15 22:22:26 +0200

    fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { NavBar } from "@/components/navBar";
 export default function Home() {
   return (
     <div className="flex justify-center items-center">
-      <NavBar className="fixed" />
+    {/*   <NavBar className="fixed" /> */}
       <h1 className="text-9xl font-bold absolute">Hello World !</h1>
       <Canvas className="w-full" />
     </div>
