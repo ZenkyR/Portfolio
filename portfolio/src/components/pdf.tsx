9096d73346868c06c6dab38fffced7dddeac1f6d
Zenky <kezon.lacheteau@epitech.eu>
2023-06-21 00:19:24 +0200

    add aplication/pdf

--- portfolio/src/components/pdf.tsx
+++ portfolio/src/components/pdf.tsx
@@ -3,7 +3,7 @@
 export const Pdf = () => {
   return (
     <>
-      <embed src="/Kezon-Lacheteau-CV.pdf" width="750" height="700"></embed>
+      <embed src="/Kezon-Lacheteau-CV.pdf" width="750" height="700"  type="application/pdf"></embed>
     </>
   );
 };
